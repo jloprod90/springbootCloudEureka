f21fae547c047566986b8fe14510611927b99db9
jloprod90 <55410301+jloprod90@users.noreply.github.com>
2023-06-21 12:39:58 +0200

    init project eureka-server

---
+++ springboot-service-eureka-server/src/main/java/com/formacionbanca/springbootserviceeurekaserver/SpringbootServiceEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbanca.springbootserviceeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceEurekaServerApplication.class, args);
+	}
+
+}
