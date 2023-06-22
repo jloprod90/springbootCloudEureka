bfbb32492d7a5fc2c5ea61d77a9c0b8d4ad03c53
jloprod90 <55410301+jloprod90@users.noreply.github.com>
2023-06-22 13:23:24 +0200

    running gateway eureka and microservices

--- springboot-service-gateway-server/src/main/java/com/formacionbanca/springbootservicegatewayserver/filters/ExampleGlobalFilters.java
+++ springboot-service-gateway-server/src/main/java/com/formacionbanca/springbootservicegatewayserver/filters/ExampleGlobalFilters.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -13,7 +12,7 @@ import reactor.core.publisher.Mono;
 @Component
 public class ExampleGlobalFilters implements GlobalFilter {
 
-    private Logger logger = LoggerFactory.getLogger(ExampleGlobalFilters.class);
+    private final Logger logger = LoggerFactory.getLogger(ExampleGlobalFilters.class);
 
 
     @Override
@@ -23,7 +22,7 @@ public class ExampleGlobalFilters implements GlobalFilter {
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
             logger.info("ejecutando filtro post");
             exchange.getResponse().getCookies().add("color", ResponseCookie.from("color","rojo").build());
-            exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+            //exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
         }));
     }
 }
--- springboot-service-products/src/main/java/com/formacionbanca/springbootserviceproducts/controllers/ProductController.java
+++ springboot-service-products/src/main/java/com/formacionbanca/springbootserviceproducts/controllers/ProductController.java
@@ -3,22 +3,28 @@ package com.formacionbanca.springbootserviceproducts.controllers;
 import com.formacionbanca.springbootserviceproducts.models.entities.Product;
 import com.formacionbanca.springbootserviceproducts.services.ProductService;
 import com.formacionbanca.springbootserviceproducts.services.ProductServiceImpl;
+import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
 public class ProductController {
 
     // inyect env (port) from application properties
-    @Value("${server.port}")
-    private Integer port;
+    @Autowired
+    private Environment env;
+
+    //@Value("${server.port}")
+    //private Integer port;
 
     @Autowired
     private ProductServiceImpl productService;
@@ -27,13 +33,18 @@ public class ProductController {
     @GetMapping("/products")
     public List<Product> getProducts() {
 
-        return productService.findAllProducts().stream().peek(p -> p.setPort(port)).collect(Collectors.toList());
+        return productService.findAllProducts().stream().map(p -> {
+            //p.setPort(port);
+            p.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+            return p;
+        }).collect(Collectors.toList());
     }
 
     @GetMapping("/products/{id}")
     public Product getProductById(@PathVariable("id") Long productId) {
         Product product =productService.findProductById(productId);
-        product.setPort(port);
+        product.setPort(Integer.parseInt(Objects.requireNonNull(env.getProperty("local.server.port"))));
+        //product.setPort(port);
         return product;
     }
 
