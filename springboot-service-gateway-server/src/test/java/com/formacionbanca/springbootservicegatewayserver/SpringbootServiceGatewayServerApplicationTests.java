8384411b7e489614c7d4d783e4008dca9354966f
jloprod90 <55410301+jloprod90@users.noreply.github.com>
2023-06-22 10:09:16 +0200

    testing run gateway-server

---
+++ springboot-service-gateway-server/src/test/java/com/formacionbanca/springbootservicegatewayserver/SpringbootServiceGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbanca.springbootservicegatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
