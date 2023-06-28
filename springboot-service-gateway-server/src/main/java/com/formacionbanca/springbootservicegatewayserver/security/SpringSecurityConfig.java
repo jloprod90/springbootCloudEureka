580f8d7050d9affc2e9b018249f2655d152eaad4
jloprod90 <55410301+jloprod90@users.noreply.github.com>
2023-06-28 18:44:08 +0200

    fix gateway SpringSecurityConfig

---
+++ springboot-service-gateway-server/src/main/java/com/formacionbanca/springbootservicegatewayserver/security/SpringSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.formacionbanca.springbootservicegatewayserver.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+    @Autowired
+    private JwtAuthenticationFilter authenticationFilter;
+
+    @Bean
+    public SecurityWebFilterChain configure(ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .pathMatchers("/api/serviceOauth/oauth/**").permitAll()
+                .pathMatchers(HttpMethod.GET, "/api/serviceProducts/products",
+                        "/api/serviceItems/items",
+                        "/api/serviceUsers/users",
+                        "/api/serviceItems/items/{id}/amount/{amount}",
+                        "/api/serviceProducts/products/{id}").permitAll()
+                .pathMatchers(HttpMethod.GET,"/api/serviceUsers/users/{id}").hasAnyRole("ADMIN","USER")
+                .pathMatchers("/api/serviceProducts/**", "/api/serviceItems/**", "/api/serviceUsers/**").hasRole("ADMIN")
+                .anyExchange().authenticated()
+                .and().addFilterAt(authenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .csrf().disable()
+                .build();
+    }
+
+}
--- springboot-service-oauth/src/main/java/com/formacionbanca/springbootserviceoauth/clients/UserFeignClient.java
+++ springboot-service-oauth/src/main/java/com/formacionbanca/springbootserviceoauth/clients/UserFeignClient.java
@@ -2,13 +2,15 @@ package com.formacionbanca.springbootserviceoauth.clients;
 
 import com.formacionbanca.springbootservicecommonsusers.models.entity.User;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "service-users")
 public interface UserFeignClient {
 
     @GetMapping("/users/search/buscar-username")
     public User findUserByUsername(@RequestParam String username);
+
+    @PutMapping("/users/{id}")
+    public User update(@RequestBody User user, @PathVariable Long id);
 
 }
