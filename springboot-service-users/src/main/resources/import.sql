74b510d2e4b3c5eb916cb6379a807105ce6885a5
jloprod90 <55410301+jloprod90@users.noreply.github.com>
2023-06-29 16:31:03 +0200

    implements database postgresql 'users'

--- springboot-service-users/src/main/resources/import.sql
+++ springboot-service-users/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (username, password, status, name, surname, email) VALUES ('pepeUser', '$2a$10$WI8SZ/sow7sq9YG3jCgllucwWYwCuJNe9/69kB1apXE5HUA8nvKSG', 1, 'pepe', 'pepon', 'pepe@gmail');
-INSERT INTO users (username, password, status, name, surname, email) VALUES ('admin', '$2a$10$WXAfOeVo6qUMblMqRmAbhOf3OkolHdYx6L32sRQnEv2lvZJpB6uwa', 1, 'admin', 'admin', 'admin@gmail');
+INSERT INTO users (username, password, status, name, surname, email) VALUES ('pepeUser', '$2a$10$WI8SZ/sow7sq9YG3jCgllucwWYwCuJNe9/69kB1apXE5HUA8nvKSG', true, 'pepe', 'pepon', 'pepe@gmail');
+INSERT INTO users (username, password, status, name, surname, email) VALUES ('admin', '$2a$10$WXAfOeVo6qUMblMqRmAbhOf3OkolHdYx6L32sRQnEv2lvZJpB6uwa', true, 'admin', 'admin', 'admin@gmail');
 
 INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
 INSERT INTO roles (name) VALUES ('ROLE_USER');
